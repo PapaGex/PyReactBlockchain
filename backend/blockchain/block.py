71e7c45af1b3aaaa4a2a8ac214483da19560ed0f
Jonathan Lang <geeksgeckosandgoodvibes@gmail.com>
2022-03-17 00:20:24 -0400

    implemented difficulty adjustment and hex to binary

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,6 +1,7 @@
 import time
 
 from backend.util.crypto_hash import crypto_hash
+from backend.config import MINE_RATE
 
 GENESIS_DATA = {
     'timestamp': 1,
@@ -45,13 +46,14 @@ class Block:
         """
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        difficulty = last_block.difficulty
+        difficulty = Block.adjust_difficulty(last_block, timestamp)
         nonce = 0
         hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
         while hash[0:difficulty] != '0' * difficulty:
             nonce += 1
             timestamp = time.time_ns()
+            difficulty = Block.adjust_difficulty(last_block, timestamp)
             hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
         return Block(timestamp, last_hash, hash, data, difficulty, nonce)
@@ -61,9 +63,18 @@ class Block:
 
         return Block(**GENESIS_DATA)
 
+    @staticmethod
+    def adjust_difficulty(last_block, new_timestamp):
+        if (new_timestamp - last_block.timestamp) < MINE_RATE:
+            return last_block.difficulty +1
+        if (last_block.difficulty - 1) > 0:
+            return last_block.difficulty - 1
+
+        return 1
+
 def main():
     genesis_block = Block.genesis()
-    block = Block.mine_block(genesis_block, 'foo')
+    block = Block.mine_block(genesis_block, 'Blood')
     print(f'block: {block}')
 
 if __name__ == '__main__':
--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -1,4 +1,8 @@
+import time
+
 from backend.blockchain.block import Block, GENESIS_DATA
+from backend.config import MINE_RATE, SECONDS
+
 
 def test_mine_block():
     last_block = Block.genesis()
@@ -12,5 +16,34 @@ def test_mine_block():
 
 def test_genesis():
     genesis = Block.genesis()
+
     for key, value in GENESIS_DATA.items():
         getattr(genesis, key) == value
+
+def test_quickly_mined_block():
+    last_block = Block.mine_block(Block.genesis(), 'Blood')
+    mined_block = Block.mine_block(last_block, 'bar')
+
+    assert mined_block.difficulty == last_block.difficulty + 1
+
+def test_slowly_mined_block():
+    last_block = Block.mine_block(Block.genesis(), 'Blood')
+    time.sleep(MINE_RATE/ SECONDS)
+    mined_block = Block.mine_block(last_block, 'bar')
+
+    assert mined_block.difficulty == last_block.difficulty -1
+
+def test_mined_block_difficulty_limits_at_1():
+    last_block = Block(
+        time.time_ns(),
+        'test_last_hash',
+        'test_hash',
+        'test_data',
+        1,
+        0
+    )
+
+    time.sleep(MINE_RATE / SECONDS)
+    mined_block = Block.mine_block(last_block, 'bar')
+
+    assert mined_block.difficulty == 1
