ab863c2f78a212b7f920a5c991bf2e4b190f5493
Jonathan Lang <geeksgeckosandgoodvibes@gmail.com>
2022-03-20 17:41:34 -0400

    finished implementing pubsub

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -1,3 +1,5 @@
+import time
+
 from pubnub.pubnub import PubNub
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.callbacks import SubscribeCallback
@@ -6,20 +8,29 @@ from pubnub.callbacks import SubscribeCallback
 pnconfig = PNConfiguration()
 pnconfig.subscribe_key = 'sub-c-c23d907a-a890-11ec-8a23-de1bbb7835db'
 pnconfig.publish_key = 'pub-c-28247e2b-a390-49a9-8c99-816245e91a8f'
-pubnub = PubNub(pnconfig)
+# pubnub = PubNub(pnconfig)
 
 TEST_CHANNEL = 'TEST_CHANNEL'
 
-pubnub.subscribe().channels([TEST_CHANNEL]).execute()
-
 class Listener(SubscribeCallback):
     def message(self, pubnub, message_object):
-        print(f'\n-- Incoming Message_Object: {message_object}')
+        print(f'\n-- Channel: {message_object.channel} | Message: {message_object.message}')
 
-pubnub.add_listener(Listener())
+class PubSub():
+    def __init__(self):
+        self.pubnub = PubNub(pnconfig)
+        self.pubnub.subscribe().channels([TEST_CHANNEL]).execute()
+        self.pubnub.add_listener(Listener())
+
+    def publish(self, channel, message):
+        self.pubnub.publish().channel(channel).message(message).sync()
 
 def main():
-    pubnub.publish().channel(TEST_CHANNEL).message({ 'Blood': 'bar' }).sync()
+    pubsub = PubSub()
+
+    time.sleep(1)
+
+    pubsub.publish(TEST_CHANNEL,{'Blood': 'Lines'})
 
 if __name__ == '__main__':
     main()
